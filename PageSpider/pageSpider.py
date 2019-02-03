5b0327e9777de677c7a8760575eebd5391cb6f5c
Suraj <yadav837krp@gmail.com>
2019-02-03 16:31:09 +0530

    initial commit

---
+++ PageSpider/pageSpider.py
@@ -0,0 +1,17 @@
+
+import argparse
+import os
+
+def main(database: str, urlListFile: str):
+    print("we are going to work with" +database)
+    print("we are going to scan" +urlListFile)
+
+
+if  __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-db", "--database", help="SQLite File Name")
+    parser.add_argument("-i", "--input", help="File Containing urls to read")
+    args = parser.parse_args()
+    database_file = args.database
+    input_file = args.input
+    main(database=database_file, urlListFile=input_file)
